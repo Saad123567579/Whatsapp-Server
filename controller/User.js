ab865f1590a9827daa919a03efd6aca5d761c479
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-08-16 23:14:06 +0500

    redux

--- controller/User.js
+++ controller/User.js
@@ -9,3 +9,35 @@ export const checkAuth = async(req,res,next) => {
     return res.status(200).json({status:true});
 
 }
+
+export const createUser = async (req, res) => {
+    const prisma = getprismaClient();
+  
+    try {
+      const { name, email, image,about } = req.body;
+  
+      // Check if user with the same email already exists
+      const existingUser = await prisma.user.findUnique({
+        where: { name },
+      });
+  
+      if (existingUser) {
+        return res.status(400).json({ message: 'User with this name already exists' });
+      }
+  
+      // Create the new user
+      const newUser = await prisma.user.create({
+        data: {
+          name,
+          email,
+          about,
+          image, // You should hash the password before saving it to the database
+        },
+      });
+  
+      res.status(201).json({ message: 'User created successfully', user: newUser });
+    } catch (error) {
+      console.error('Error creating user:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
--- router/User.js
+++ router/User.js
@@ -1,6 +1,7 @@
-import {checkAuth} from "../controller/User.js";
+import {checkAuth,createUser} from "../controller/User.js";
 import express from "express";
 const router = express.Router();
 router.post('/check',checkAuth);
+router.post('/createuser',createUser);
 
 export default router;
