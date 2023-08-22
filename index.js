ac492f0eff295ea03d96657b1e759b2521fda734
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-08-22 13:49:38 +0500

    typing and status functionality implemented

--- index.js
+++ index.js
@@ -23,18 +23,46 @@ app.use('/message',messageRouter);
 
 const server = app.listen(process.env.PORT,()=>console.log("Server Started"));
 
-// const io = new Server(server,{
-//     cors:{origin:"http://localhost:3000"}
-// });
+const io = new Server(server,{
+    cors:{origin:"http://localhost:3000"}
+});
 global.onlineUsers = new Map();
 
-// io.on("connection",(socket)=>{
-//     socket.on("error", (error) => {
-//         console.error("Socket error:", error);
-//       });
-//     console.log("A new user has connected ",socket.id);
-// })
-// io.on('disconnect', (socket) => {
-//     // Perform cleanup tasks or logging
-//     console.log('A client has disconnected:', socket.id);
-// });
+io.on("connection",(socket)=>{
+    var socketId = socket.id;
+    socket.on("error", (error) => {console.error("Socket error:", error);});
+    global.chatSocket = socket;  
+    console.log("A new user has connected ",socket.id);
+    socket.on("adduser",(userid)=>{
+        onlineUsers.set(userid,socket.id);
+        console.log(onlineUsers);
+        socket.broadcast.emit("getOnlineusers",Array.from(onlineUsers.keys()));
+    })
+    socket.on("disconnect", () => {
+        onlineUsers.forEach((value, key) => {
+            if (value === socketId) {
+                onlineUsers.delete(key);
+                console.log("The id ", key, " was found and has been removed from the connection");
+                console.log(onlineUsers);
+                socket.broadcast.emit("getOnlineusers",Array.from(onlineUsers.keys()));
+            } else {console.log("no socket id could be found");}
+        });
+    })
+    socket.on("getcurrentusers",(userid)=>{
+        if(onlineUsers.has(userid)){
+            io.to(onlineUsers.get(userid)).emit("herecurrentusers",Array.from(onlineUsers.keys()))
+        }
+    })
+    socket.on("typing",(obj)=>{
+        const {from,to} = obj;
+        if(onlineUsers.has(from) && onlineUsers.has(to)){
+            io.to(onlineUsers.get(to)).emit("typing");
+        }
+        setTimeout(() => {
+            io.to(onlineUsers.get(to)).emit("herecurrentusers",Array.from(onlineUsers.keys()))
+        }, 2000);
+    })
+   
+    
+
+})
